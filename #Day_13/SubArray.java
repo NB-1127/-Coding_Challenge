d4b6e7aafac23750eb745863c0500ce5e01e825d
NB-1127 <nehabobade55@gmail.com>
2024-06-04 21:41:23 +0530

    new added file

---
+++ #Day_13/SubArray.java
@@ -0,0 +1,21 @@
+public class SubArray {
+
+    public static void main(String[] args) {
+        
+        int arr[]={5,10,15,20};
+        
+        for (int i = 0; i < arr.length; i++) {
+            
+            for (int j = i; j < arr.length; j++) {
+                
+                for (int k = i; k <=j; k++) {
+
+                    System.out.print(arr[k]+" ");
+                    
+                }
+                System.out.println();
+            }
+        }
+    }
+    
+}
