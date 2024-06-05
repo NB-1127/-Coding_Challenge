e8d12c843b56e0d10291ba558480a470f2dd2b0b
NB-1127 <nehabobade55@gmail.com>
2024-06-05 19:20:03 +0530

    some changes made

--- #Day_13/LinearMenu.java
+++ #Day_13/LinearMenu.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class LinearMenu {
 
 
@@ -5,7 +7,7 @@ public class LinearMenu {
     public static int findString(String menu[], String key){
 
         for (int i = 0; i < menu.length; i++) {
-            if (menu[i]== key) {
+            if (menu[i].equals(key)) {
                 return i;
             }
         }
@@ -13,8 +15,11 @@ public class LinearMenu {
     }
 
     public static void main(String[] args) {
-        String menu[]={"dosa","Chole bhature","samosa"};
-        String key= "samosa";
+        String menu[]={"dosa","Chole bhature","samosa","pav Bhaji"};
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter a menu");
+        String key= sc.next();
 
         int index=findString(menu, key);
         if (index== -1) {
