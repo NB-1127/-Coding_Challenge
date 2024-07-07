8f415999585c9d44515f77baef43b11abdd078cc
NB-1127 <nehabobade55@gmail.com>
2024-07-07 21:28:17 +0530

    add new day

---
+++ #Day_38/BeautifulArrayList.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Demo {
+
+    
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        ArrayList<Integer> ans= new ArrayList<>();
+
+        int n=sc.nextInt();
+
+        int rem=n-1;
+        ans.add(1);
+        
+
+        for (int i = 1; i < rem; i++) {
+         
+            ArrayList<Integer> temp=new ArrayList<>();
+
+            for(int ele : ans){
+                if (ele *2<=n) {
+                    temp.add(2*ele);   
+                }
+            }
+            for(int ele : ans){
+                if (ele *2-1<=n) {
+                    temp.add(2*ele-1);   
+                }
+            }
+            ans=temp;
+        }
+        System.out.println(ans);
+    }
+}
