dd4fa54d4a4a675b6869e0699a35bf1e02de3838
NB-1127 <nehabobade55@gmail.com>
2024-07-30 23:04:57 +0530

    another day

---
+++ #Day_57/LinkedList.java
@@ -0,0 +1,51 @@
+public class LinkedList {
+
+    public static class Node{
+
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+
+    public void addFirst(int data){
+
+        Node newNode=new Node(data);
+        if (head == null) {
+            head= tail= newNode;
+            return;
+        }
+        newNode.next=head;
+        head= newNode; 
+    }
+    public void print(){
+        Node temp= head;
+        if (head == null) {
+            System.out.print("List is empty ");
+        }
+        while (temp != null) {
+            System.out.print(temp.data+ "->");
+            temp= temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        
+        LinkedList list=new LinkedList();
+
+        list.print();
+        list.addFirst(6);
+        list.addFirst(5);
+        list.addFirst(4);
+        list.addFirst(3);
+        list.addFirst(2);
+        list.addFirst(1);
+        list.print();
+    }
+}
